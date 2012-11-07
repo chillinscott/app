76c7036120f810fab8fd94569c5042a1b124f0e9
chillinscott <marcus.scott.burleigh@gmail.com>
2012-11-07 16:05:47 -0600

    Pushing new changes

--- source/app/Calculator.cs
+++ source/app/Calculator.cs
@@ -1,35 +1,40 @@
 ﻿using System;
 using System.Data;
+using System.Security;
+using System.Threading;
 
 namespace app
 {
-  public class Calculator
-  {
-    IDbConnection connection;
-
-    public Calculator(IDbConnection connection)
+    public class Calculator
     {
-      this.connection = connection;
+        IDbConnection connection;
+
+        public Calculator(IDbConnection connection)
+        {
+            this.connection = connection;
+        }
+
+        public int add(int i, int i1)
+        {
+            if (i < 0 || i1 < 0)
+                throw new ArgumentException("Negatives aren't allowed");
+
+            using (connection)
+            using (var command = connection.CreateCommand())
+            {
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+
+            return i + i1;
+        }
+
+        public void shut_off()
+        {
+            if (!Thread.CurrentPrincipal.IsInRole("User"))
+            {
+                throw new SecurityException("Unauthorized");
+            }
+        }
     }
-
-    public int add(int i, int i1)
-    {
-      if (i < 0 || i1 < 0)
-        throw new ArgumentException("Negatives aren't allowed");
-
-      using (connection)
-      using (var command = connection.CreateCommand())
-      {
-        connection.Open();
-        command.ExecuteNonQuery();
-      }
-
-      return i + i1;
-    }
-
-    public void shut_off()
-    {
-      throw new NotImplementedException();
-    }
-  }
 }
