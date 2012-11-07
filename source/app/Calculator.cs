5e77ea57f1447222498b9622f1fe84110c0bfea2
chillinscott <marcus.scott.burleigh@gmail.com>
2012-11-07 15:37:00 -0600

    Pushing new changes

--- source/app/Calculator.cs
+++ source/app/Calculator.cs
@@ -1,13 +1,21 @@
 ﻿using System;
+using System.Data;
 
 namespace app
 {
   public class Calculator
   {
+      readonly IDbConnection connection;
+
+      public Calculator(IDbConnection connection)
+      {
+          this.connection = connection;
+      }
     public int add(int i, int i1)
     {
       if(i < 0 || i1 < 0)
           throw new ArgumentException("Negatives aren't allowed");
+      connection.Open();
       return i + i1;
     }
   }
